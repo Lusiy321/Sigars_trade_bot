979e829cb9aa725cc1d37158a95af63ac3ac5bd6
Volodymyr Lysenko <lusiy321@gmail.com>
2024-05-09 16:09:14 +0300

    fixes

--- src/app.service.ts
+++ src/app.service.ts
@@ -17,7 +17,7 @@ export class AppService {
       if (chatId.toString() === adminChatId) {
         await this.bot.sendMessage(chatId, `Привіт! Admin`, {
           reply_markup: {
-            keyboard: [['Список товаров']],
+            keyboard: [['Список товарів']],
             resize_keyboard: true,
           },
         });
@@ -27,7 +27,7 @@ export class AppService {
           `Привіт! ${msg.from.first_name}.\n` + startMsg,
           {
             reply_markup: {
-              keyboard: [['Список товаров']],
+              keyboard: [['Список товарів']],
               resize_keyboard: true,
             },
           },
@@ -35,27 +35,23 @@ export class AppService {
       }
     });
 
-    this.bot.onText(/Список товаров/, async (msg: any) => {
+    this.bot.onText(/Список товарів/, async (msg: any) => {
       const chatId = msg.chat.id;
       this.bot.sendMessage(chatId, product, {
         reply_markup: {
-          keyboard: [['Список товаров']],
+          keyboard: [['Список товарів']],
           resize_keyboard: true,
         },
       });
     });
-    // Обработчик всех входящих сообщений
-    this.bot.on('message', (msg) => {
+
+    this.bot.on('message', (msg: any) => {
       try {
         const chatId = msg.chat.id;
-
-        // Проверяем, является ли отправитель администратором
         const isAdmin = chatId.toString() === adminChatId;
-
-        // Если отправитель - не администратор, пересылаем сообщение администратору
         if (!isAdmin) {
-          if (msg.text === 'Список товаров' || msg.text === '/start') {
-            console.log('User whatch');
+          if (msg.text === 'Список товарів' || msg.text === '/start') {
+            console.log(`User ${msg.from.first_name} whatch`);
           } else {
             this.bot.sendMessage(
               adminChatId,
@@ -63,41 +59,18 @@ export class AppService {
             );
           }
         } else if (isAdmin || msg.reply_to_message.text !== undefined) {
-          if (msg.text !== '/start' && msg.text !== 'Список товаров') {
+          if (msg.text !== '/start' && msg.text !== 'Список товарів') {
             const adminReply = msg.reply_to_message.text;
             const [userChatId] = adminReply.split(' ');
-            // Отправляем сообщение пользователю
             this.bot.sendMessage(userChatId, `${msg.text}`);
           }
         }
       } catch (e) {
-        this.bot.sendMessage(adminChatId, 'Привет Админ');
+        this.bot.sendMessage(adminChatId, 'Привіт Адмін');
       }
     });
 
-    this.bot.onText(/\/reply (.+)/, (msg, match) => {
-      // Извлекаем текст ответа администратора
-      const adminReply = match[1];
-
-      // Проверяем, что отправитель - администратор
-      const isAdmin = msg.chat.id.toString() === adminChatId;
-
-      if (isAdmin) {
-        // Извлекаем ID пользователя и текст сообщения из команды /reply
-        const [userChatId, userMessage] = adminReply.split(' ');
-
-        // Отправляем сообщение пользователю
-        this.bot.sendMessage(userChatId, userMessage);
-      } else {
-        // Если отправитель не администратор, отправляем сообщение об ошибке
-        this.bot.sendMessage(msg.chat.id, 'Вы не администратор!');
-      }
-    });
-
-    // Обработчик для ответов администратора
-
-    // Отслеживаем ошибки
-    this.bot.on('polling_error', (error) => {
+    this.bot.on('polling_error', (error: any) => {
       console.error(error);
     });
   }
