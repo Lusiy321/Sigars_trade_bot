10984a3b75da75e10fa36ec4a6dad65da56266d9
Volodymyr Lysenko <lusiy321@gmail.com>
2024-05-15 17:33:29 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 
 async function start() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(express), {
+    cors: true,
+  });
+
+  app.enableCors({});
   await app.listen(3000, async () => {
     console.log(`Server started on port = http://localhost:3000`);
   });
